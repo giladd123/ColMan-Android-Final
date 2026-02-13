780c00336a97e6e2a3fcf899b3b49e7ae7c7f261
Gilad Tidhar <giladtidhar19@gmail.com>
2026-02-13 14:42:58 +0200

    added bottom bar

--- app/src/main/java/com/example/androidfinalproject/MainActivity.kt
+++ app/src/main/java/com/example/androidfinalproject/MainActivity.kt
@@ -2,11 +2,14 @@ package com.example.androidfinalproject
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.example.androidfinalproject.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_nav)
+        bottomNav.setOnItemSelectedListener { true }
     }
 }
